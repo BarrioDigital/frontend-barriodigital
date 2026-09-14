0bd37646252cff49263955ffd280f6cc29f9bb18
diat07 <arayadiego6467@gmail.com>
2026-09-14 19:05:57 -0300

    axios

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// Si no existe VITE_API_BASE_URL, trabaja localmente usando el proxy de Vite.
+// Cuando tengan API Gateway, solo se configura esa variable.
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+
 export const axiosClient = axios.create({
-  baseURL: '',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
